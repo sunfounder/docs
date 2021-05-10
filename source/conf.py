aa6443b897150f7a0b110c54aebbc8a002b8a98b
zhanzghiyong0410 <2556703570@qq.com>
2021-05-10 17:22:29 +0800

    theme furo

--- source/conf.py
+++ source/conf.py
@@ -59,7 +59,7 @@ html_logo = '_static/img/sunfounder.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_theme = 'karma_sphinx_theme'
+html_theme = 'furo'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 source_parsers = {
