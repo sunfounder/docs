092e43a0a0d7708af2d30e686af45d5bea5a76b9
zhanzghiyong0410 <2556703570@qq.com>
2021-05-10 17:03:49 +0800

    test

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ html_logo = '_static/img/sunfounder.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = 'karma_sphinx_theme'
-##html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 source_parsers = {
     '.md':CommonMarkParser,
