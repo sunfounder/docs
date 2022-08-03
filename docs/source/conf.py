738a0b56e779737aa966ae5e30496b0bf31ebe0a
xiemeiping <1035791926@qq.com>
2022-08-03 10:41:43 +0800

    change the navigation bar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,8 +64,8 @@ import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_js_files = [
-    'https://ezblock.cc/readDocFile/topHead.js',
+    'https://ezblock.cc/readDocFile/custom.js',
 ]
 html_css_files = [
-    'https://ezblock.cc/readDocFile/topHead.css',
+    'https://ezblock.cc/readDocFile/custom.css',
 ]
